b75ed495b12928a6d6b1b9fc666b8f0eeef29c5e
lin_wsl <hao.lin0728@gmail.com>
2024-09-18 14:23:18 -0400

    added logic to dynamically add ./Project to $PYTHONPATH in Project.__init__.py; adjusted render.yaml as needed & removed mispelled requirements file

--- Project/__init__.py
+++ Project/__init__.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app import app
 
 if __name__ == "__main__":
+    print("Flask app =" app)
     app.run()
